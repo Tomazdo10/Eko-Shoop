4f72e9d3dcc86c0c7f15226005a3d014df2f4216
Tomazdo10 <tomaz.dobnik@gmail.com>
2021-03-30 13:26:46 +0000

    added images to products.json

--- home/models.py
+++ home/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(
-        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+        'category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField(max_length=500)
     price = models.DecimalField(
@@ -24,6 +24,7 @@ class Product(models.Model):
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(
         max_length=1024)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
