36c0ae9825dbc74ffb8dc894de8de5329ebe8c69
Tomazdo10 <tomaz.dobnik@gmail.com>
2021-04-26 08:44:09 +0000

    added shooping bag urls and templates

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Product, Category
 
-#Register your models here
+# Register your models here
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -11,16 +12,17 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'rating',
         'image',
-        'img_url',
     )
 
     ordering = ('category',)
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
