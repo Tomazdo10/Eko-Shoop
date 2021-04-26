bbc8f767429f3373c08095487932db905e03f6dd
Tomazdo10 <tomaz.dobnik@gmail.com>
2021-04-26 09:31:53 +0000

    applied migration for views.py in profile account

--- products/views.py
+++ products/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
+# Create your views here.
 
 def all_products(request):
-    """ A view to return index page """
+    """ A view to show all products, including sorting and search queries """
 
-    products = Product.object.all()
+    products = Product.objects.all()
     query = None
     categories = None
     sort = None
-    directions = None
+    direction = None
 
     if request.GET:
         if 'sort' in request.GET:
@@ -20,32 +23,33 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
-                    sortkey = 'f-{sortkey}'
+                    sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-    if request.GET:
+            
         if 'category' in request.GET:
-            categories = request.GET['category'].split('/')
+            categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            categories = Category.object.filter(name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't search any criteria!")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
-            queries = Q(name_icontaines=query) | Q(description_icontaines=query)
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
-        'searc_term': query,
+        'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
