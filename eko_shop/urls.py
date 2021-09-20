99d2db0fc05a6d297b3b48c4e0d23c21319c3beb
Tomazdo10 <tomaz.dobnik@gmail.com>
2021-09-20 15:51:13 +0000

    added base.html in allauth account and change the content block with inner content

--- eko_shop/urls.py
+++ eko_shop/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
-    path('profiles/', include('profiles.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
